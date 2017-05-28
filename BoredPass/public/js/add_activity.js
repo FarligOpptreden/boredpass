7496fe5a4a362297a3d0720c8f5afa85f0ba05bf
Werner van der Vyver <wernervdv@gmail.com>
2017-05-28 19:49:11 +0200

    Adding an activity first wizard step done with styling and transitions

---
+++ BoredPass/public/js/add_activity.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    $("#add-activity > a").click(function () {
+        $.ajax({
+            url: "/activity/add",
+            method: "get",
+            dataType: "html"
+        }).done(function (data) {
+            var overlay = $(data);
+            Shared.showOverlay({
+                content: overlay,
+                callback: function () {
+                    overlay.find(".close").click(function () {
+                        Shared.hideOverlay();
+                    });
+                }
+            });
+        });
+    });
+    setTimeout(function () {
+        $("#add-activity > a").trigger("click");
+    }, 1000);
+});
---
+++ BoredPass/server/controllers/html/activity.js
@@ -0,0 +1,7 @@
+﻿import Controller from '../../../handlr/Controller';
+
+export default new Controller('/activity')
+    // Show default index page
+    .handle({ route: '/add', method: 'get', produces: 'html' }, (req, res) => {
+        res.render('partials/add_activity', null);
+    });

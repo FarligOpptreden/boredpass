dac2c4f0ed9dfb7674f1dd59c980b41b739506d4
Werner van der Vyver <wernervdv@gmail.com>
2017-06-11 14:45:57 +0200

    Fixed navigation between tabs when adding an activity and added the necessary events and styling. Cleaned up some styling bugs in mobile views.

--- BoredPass/public/js/add_activity.js
+++ BoredPass/public/js/add_activity.js
@@ -9,6 +9,7 @@
             Shared.showOverlay({
                 content: overlay,
                 callback: function () {
+                    activityEvents();
                     overlay.find(".close").click(function () {
                         Shared.hideOverlay();
                     });
@@ -16,7 +17,41 @@
             });
         });
     });
+    var activityEvents = function () {
+        $(".add-activity .next, .add-activity .prev").click(function () {
+            var current = $(".step:not(.shift)");
+            var prev = current.prevAll(".step");
+            var next = current.nextAll(".step");
+            var direction = $(this).hasClass("next") ? "next" : "prev";
+            var activeBreadcrumb = $(".add-activity .breadcrumb a.active");
+            activeBreadcrumb.removeClass("active");
+            if (direction === "next")
+                activeBreadcrumb.next("a").addClass("active");
+            else
+                activeBreadcrumb.prev("a").addClass("active");
+            current.addClass("shift " + (direction === "next" ? "left1" : "right1"));
+            prev.each(function () {
+                var amount = $(this)[0].className.replace(/step|shift|left/g, "");
+                amount = parseInt(amount.trim());
+                $(this).removeClass("shift left" + amount);
+                if (direction === "prev" && amount == 1)
+                    return;
+                $(this).addClass("shift left" + (direction === "prev" ? --amount : ++amount));
+            });
+            next.each(function () {
+                var amount = $(this)[0].className.replace(/step|shift|right/g, "");
+                amount = parseInt(amount.trim());
+                $(this).removeClass("shift right" + amount);
+                if (direction === "next" && amount == 1)
+                    return;
+                $(this).addClass("shift right" + (direction === "next" ? --amount : ++amount));
+            });
+        });
+        $(".add-activity .done").click(function () {
+            $(this).closest(".overlay-content").find(".close").trigger("click");
+        });
+    };
     setTimeout(function () {
-        //$("#add-activity > a").trigger("click");
-    }, 1000);
+        $("#add-activity > a").trigger("click");
+    }, 500);
 });

1d92e70edd2e9c296a7a8f659cf3c882dcfad123
Werner van der Vyver <wernervdv@gmail.com>
2017-07-15 17:12:50 +0200

    Latest work based on Jorge's designs

---
+++ BoredPass/public/js/listing.js
@@ -0,0 +1,28 @@
+﻿var Listing = {};
+
+$(document).ready(function () {
+    var doScroll = function (factor, numVisible) {
+        var count = $(".photos > div > .photo").length;
+        var currentScroll = parseInt($(".photos > div").data("current-scroll"), 10);
+        $(".photos > div").data("current-scroll", (factor > 0 ? ++currentScroll : --currentScroll));
+        $(".photos .photo").css("transform", "translateX(" + (-currentScroll * 100) + "%)");
+        if (count - numVisible > currentScroll) {
+            $(".photos .next").removeClass("hidden");
+        } else {
+            $(".photos .next").addClass("hidden");
+        }
+        if (currentScroll > 0) {
+            $(".photos .prev").removeClass("hidden");
+        }
+        else {
+            $(".photos .prev").addClass("hidden");
+        }
+    };
+    $(".photos .next, .photos .prev").click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass("hidden"))
+            return;
+        doScroll($(this).hasClass("next") ? 1 : -1, 5);
+        return false;
+    });
+});
---
+++ BoredPass/server/controllers/html/listing.js
@@ -0,0 +1,12 @@
+﻿import Controller from '../../../handlr/Controller';
+
+export default new Controller('/listings')
+    .handle({ route: '/add', method: 'get', produces: 'html' }, (req, res) => {
+        res.render('partials/add_listing', null);
+    })
+    .handle({ route: '/:id', method: 'get', produces: 'html' }, (req, res) => {
+        res.render('listing', {
+            title: 'Listing Name - BoredPass',
+            moment: require('moment')
+        });
+    });

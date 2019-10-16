c98c2cac60b7f142a818b021a49b9f45c31ac6b4
Werner van der Vyver <wernervdv@gmail.com>
2019-10-16 12:01:42 +0200

    Updated middleware to include moment so the copyright year always renders correctly.

--- BoredPass/server/middleware/categories.js
+++ BoredPass/server/middleware/categories.js
@@ -2,20 +2,19 @@
 
 export default class Cache {
   static get(app) {
-    app.all("*", (req, _, next) => {
+    app.all("*", (req, _, next) =>
       TagsService.uniqueCategories().then(categories => {
-        req.listing_categories = categories.map(c => {
-          return {
-            category: c.category,
-            urlCategory: c.category
-              .toLowerCase()
-              .replace(/\s/g, "-")
-              .replace(/\&/g, "and"),
-            tags: c.tags
-          };
-        });
+        req.listing_categories = categories.map(c => ({
+          category: c.category,
+          urlCategory: c.category
+            .toLowerCase()
+            .replace(/\s/g, "-")
+            .replace(/\&/g, "and"),
+          tags: c.tags
+        }));
+        req.moment = require("moment");
         next();
-      });
-    });
+      })
+    );
   }
 }
